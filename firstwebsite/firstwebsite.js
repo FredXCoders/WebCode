8048850e3798d51e3d401a19844f1dde9965ddb1
kfp <dev@sperlazzasaurus.com>
2014-05-26 11:43:05 -0400

    made website customization easier

--- firstwebsite/firstwebsite.js
+++ firstwebsite/firstwebsite.js
@@ -2,3 +2,11 @@ function changeText(){
 	var newText =  document.getElementById("firstInput").value;
 	document.getElementById("paragraph").innerHTML = newText;	
 }
+
+jQuery("document").ready(function(){
+  jQuery.get("./info.json", function(response){
+    if(response.name){
+      jQuery("#name").html(response.name);
+    }
+  }, "json");
+});
